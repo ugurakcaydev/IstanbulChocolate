23513ba23c9bff6ce897d70db47e766e900ebb95
Ugur Akcay <akcayugur01@gmail.com>
2023-03-10 22:52:34 +0300

    aaa

---
+++ login.js
@@ -0,0 +1,8 @@
+function myFunction() {
+
+    if (this.className == "fa-solid fa-lock-open") {
+        x = "fa-solid fa-lock";
+    } else if (this.className == "fa-solid fa-lock") {
+        x = "fa-solid fa-lock-open";
+    }
+}
