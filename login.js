6e35465b040bdae5e3fead3cd96e4329acd4da89
Ugur Akcay <akcayugur01@gmail.com>
2023-03-11 12:58:09 +0300

    aa

--- login.js
+++ login.js
@@ -1,8 +1,18 @@
-function myFunction() {
+// var element = document.getElementById("password");
+// myFunction = (icon) => icon.classList.toggle('fa-eye'),
+//     element.type = (element.type == "password") ? "password" : "text"
+function showPassword(id, el) {
+    let x = document.getElementById(id)
+    if (x.type == "password") {
+        x.type = "text";
+        el.classList.toggle("fa-eye")
 
-    if (this.className == "fa-solid fa-lock-open") {
-        x = "fa-solid fa-lock";
-    } else if (this.className == "fa-solid fa-lock") {
-        x = "fa-solid fa-lock-open";
+        console.log(x.type);
+        console.log(el.classList)
+    } else {
+        x.type = "password";
+        el.classList.toggle("fa-eye")
+        console.log("else " + x.type);
+        console.log("else " + el.classList)
     }
-}
+}    
